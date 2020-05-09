eb5c55fb10762f76aa4955d43e5ed4e506918b9a
detemegandy <andreas.saltveit@gmail.com>
2020-05-09 14:15:21 +0200

    added parameter to save condition on side

--- dice.py
+++ dice.py
@@ -4,15 +4,23 @@ class dice():
         self.sides = sides
         self.sideUp = random.randint(1,self.sides)
         self.saved = False
+        
     def __repr__(self):
         return "(sides: " + str(self.sides) + ", sideUp: " + str(self.sideUp) + ", saved: " + str(self.saved)+")"
+
     def __str__(self):
         return str(self.sideUp)
+
     def roll(self):
         if self.saved == False:
             self.sideUp = random.randint(1,self.sides)
-    def save(self):
-        self.saved = True
+
+    def save(self,side):
+        if self.sideUp == side:
+            self.saved = True
+            return 1
+        return 0
+        
     def rollSaved(self):
         self.sideUp = random.randint(1,self.sides)
         self.saved == False
