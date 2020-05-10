3f1ae54966ffe2e132a0aa8d295ef33b402a64c9
detemegandy <andreas.saltveit@gmail.com>
2020-05-10 02:42:18 +0200

    changed repr for dice to only display sideup and saved state

--- dice.py
+++ dice.py
@@ -4,9 +4,9 @@ class dice():
         self.sides = sides
         self.sideUp = random.randint(1,self.sides)
         self.saved = False
-        
+
     def __repr__(self):
-        return "(sides: " + str(self.sides) + ", sideUp: " + str(self.sideUp) + ", saved: " + str(self.saved)+")"
+        return "(sideUp: " + str(self.sideUp) + ", saved: " + str(self.saved)+")"
 
     def __str__(self):
         return str(self.sideUp)
